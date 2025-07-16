db456130d9166cb33a23602bddb9fa7abc044f55
Arthur-Pereira <60709850+Arthur-Pereira-5019@users.noreply.github.com>
2025-07-16 08:44:54 -0300

    Wrapped everything inside functions, spent some time learning more about the pyautogui function and alternatives for pytesseract.

--- main.py
+++ main.py
@@ -11,94 +11,98 @@ import winsound
 
 # Basic settings
 ts.pytesseract.tesseract_cmd = 'tesseract/tesseract.exe'
-print(ts.get_languages(config=''))
 
 if not os.path.isdir('generated'):
     os.mkdir('generated')
 
-i = 0;
+i = 0
+fileName = ''
+atualDisciplina = ''
 
 # Open the discipline
-a = py.locateOnScreen('componente.png')
-x, y = py.center(a)
-py.click(x - 90, y + 25)
 
-# Go for the "Alunos" tab
-# t.sleep(2)
-# canSkip = False;
-# while not canSkip:
-# try:
-# a = py.locateOnScreen('alunos.png')
-# canSkip = True
-# except Exception as e:
-# print('Is your internet slow or there is some problem with your screen? Not found the Alunos button. Trying again in 3 seconds')
-# time.sleep(3)
-
-# x, y = py.center(a)
-# py.click(x,y)
-
-t.sleep(0.5)
-canSkip = False;
-while not canSkip:
-    try:
-        a = py.locateOnScreen('frequencia.png')
-        canSkip = True
-    except Exception as e:
-        try:
-            a = py.locateOnScreen('alunos.png')
-            x, y = py.center(a)
-            py.click(x, y)
-        except Exception as b:
-            time.sleep(1)
-
-x, y = py.center(a)
-py.click(x, y)
-t.sleep(0.5)
-
-canSkip = False
-while not canSkip:
-    try:
-        a = py.locateOnScreen('mapadefrequencia.png')
-        canSkip = True
-    except Exception as e:
-        t.sleep(0.5)
-
-x, y = py.center(a)
-py.click(x, y)
-py.scroll(-400)
-
-canSkip = False
-while not canSkip:
-    try:
-        a = py.locateOnScreen('abscensesfileref.png')
-        canSkip = True
-    except Exception as e:
-        t.sleep(0.5)
-
-printName = 'generated/disciplina' + i.__str__() + '.png'
-absencesBox = (a.left, a.top, a.width + int(a.width / 1.5), a.height)
-py.screenshot(printName, absencesBox)
-
-results = ts.image_to_string(printName, 'eng')
-# print(results)
-
-absences = results.rsplit(": ")[1].rsplit("\n")[0]
-maximum = results.rsplit(": ")[2]
-
-
-def texttreatment(a):
+def textTreatment(a):
     match a:
         case ("B"):
             return "8"
     return a
 
+def returnAbsences():
+    results = ts.image_to_string(fileName, 'eng')
 
-absences = texttreatment(absences)
-maximum = texttreatment(maximum)
-if not absences.isnumeric:
-    print("Não foi possível achar o número de faltas na disciplina " + i.__str__())
-if not absences.isnumeric:
-    print("Não foi possível achar o número máximo de faltas na disciplina " + i.__str__())
-print("O número de faltas na disciplina é " + absences.__str__() + "/" + maximum.__str__())
+    absences = results.rsplit(": ")[1].rsplit("\n")[0]
+    maximum = results.rsplit(": ")[2]
+    absences = textTreatment(absences)
+    maximum = textTreatment(maximum)
+    global i
+    if not absences.isnumeric:
+        print("Não foi possível achar o número de faltas na disciplina " + i.__str__())
+    if not absences.isnumeric:
+        print("Não foi possível achar o número máximo de faltas na disciplina " + i.__str__())
+    print("O número de faltas na disciplina é " + absences.__str__() + "/" + maximum.__str__())
+
+
+def findAndPrintAbsencesBox():
+    canSkip = False
+    while not canSkip:
+        try:
+            a = py.locateOnScreen('abscensesfileref.png')
+            canSkip = True
+        except Exception as e:
+            t.sleep(0.5)
+
+    global fileName
+    fileName = 'generated/disciplina' + i.__str__() + '.png'
+    absencesBox = (a.left, a.top, a.width + int(a.width / 1.5), a.height)
+    py.screenshot(fileName, absencesBox)
+    returnAbsences()
+
+def rollDown():
+    canSkip = False
+    while not canSkip:
+        try:
+            a = py.locateOnScreen('mapadefrequencia.png')
+            canSkip = True
+        except Exception as e:
+            t.sleep(0.5)
+
+    x, y = py.center(a)
+    py.click(x, y)
+    py.scroll(-400)
+    findAndPrintAbsencesBox()
+
+def hasNext():
+    py.click()
+    py.click()
+    py.click()
+    py.hotkey('ctrl', 'shift', 'c')
+
+
+def goToFrequncy():
+    a = py.locateOnScreen('componente.png')
+    x, y = py.center(a)
+    py.click(x - 90, y + 25)
+
+    t.sleep(0.5)
+    canSkip = False;
+    while not canSkip:
+        try:
+            a = py.locateOnScreen('frequencia.png')
+            canSkip = True
+        except Exception as e:
+            try:
+                a = py.locateOnScreen('alunos.png')
+                x, y = py.center(a)
+                py.click(x, y)
+            except Exception as b:
+                time.sleep(1)
+
+    x, y = py.center(a)
+    py.click(x, y)
+    t.sleep(0.5)
+    rollDown()
+
+
+goToFrequncy()
 
 winsound.Beep(500, 1000)
