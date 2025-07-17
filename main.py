43d466a9612aeddcfc3748af711001caf266221a
Arthur-Pereira <60709850+Arthur-Pereira-5019@users.noreply.github.com>
2025-07-17 13:44:12 -0300

    -Finished the basic functionality of the sistem. -Removed Tesseract -Created a build version -Added a greetings message -Used the greyscale config to accelerate the program speed -TODO: Optimize and add use instructions

--- main.py
+++ main.py
@@ -2,107 +2,156 @@ import string
 import time
 
 import pyautogui as py
-import pyscreeze
 import time as t
-from PIL import Image
-import pytesseract as ts
-import os
 import winsound
+import pyperclip as cb
+import sys
+import os
 
 # Basic settings
-ts.pytesseract.tesseract_cmd = 'tesseract/tesseract.exe'
 
-if not os.path.isdir('generated'):
-    os.mkdir('generated')
+
+def resource_path(relative_path):
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base_path, relative_path)
 
 i = 0
 fileName = ''
-atualDisciplina = ''
+currentDiscipline = ''
+ended = False
+print("Começou! Sigaa Killer 1.0 por Arthur Pereira a.k.a. Art5019")
 
-# Open the discipline
-
-def textTreatment(a):
-    match a:
-        case ("B"):
-            return "8"
-    return a
-
-def returnAbsences():
-    results = ts.image_to_string(fileName, 'eng')
-
-    absences = results.rsplit(": ")[1].rsplit("\n")[0]
-    maximum = results.rsplit(": ")[2]
-    absences = textTreatment(absences)
-    maximum = textTreatment(maximum)
-    global i
-    if not absences.isnumeric:
-        print("Não foi possível achar o número de faltas na disciplina " + i.__str__())
-    if not absences.isnumeric:
-        print("Não foi possível achar o número máximo de faltas na disciplina " + i.__str__())
-    print("O número de faltas na disciplina é " + absences.__str__() + "/" + maximum.__str__())
-
-
-def findAndPrintAbsencesBox():
+def returnHome():
     canSkip = False
     while not canSkip:
         try:
-            a = py.locateOnScreen('abscensesfileref.png')
+            x, y = py.locateCenterOnScreen(resource_path('images/homebutton.png'), grayscale=True)
+            canSkip = True
+        except Exception as e:
+            t.sleep(0.5)
+    py.click(x, y)
+
+def returnAbsences(results):
+    lines = results.splitlines()
+    absences = (lines[0].rsplit(": ")[1])
+    maximum = lines[1].rsplit(": ")[1]
+
+    global currentDiscipline
+    print("O número de faltas na disciplina " + currentDiscipline + " é " + absences + "/" + maximum)
+    returnHome()
+
+def extractAbsencesUsingCursor():
+    canSkip = False
+    while not canSkip:
+        try:
+            a = py.locateOnScreen(resource_path('images/abscensesfileref.png'),grayscale=True)
+
             canSkip = True
         except Exception as e:
             t.sleep(0.5)
 
-    global fileName
-    fileName = 'generated/disciplina' + i.__str__() + '.png'
-    absencesBox = (a.left, a.top, a.width + int(a.width / 1.5), a.height)
-    py.screenshot(fileName, absencesBox)
-    returnAbsences()
+    py.click(a.left, a.top)
+    py.keyDown('shift')
+    py.click(a.left+a.width + a.width/2, a.top+a.height)
+    py.keyUp('shift')
+    py.hotkey('ctrl', 'c')
+
+    returnAbsences(cb.paste())
+
 
 def rollDown():
     canSkip = False
     while not canSkip:
         try:
-            a = py.locateOnScreen('mapadefrequencia.png')
+            x, y = py.locateCenterOnScreen(resource_path('images/mapadefrequencia.png'), grayscale=True)
             canSkip = True
         except Exception as e:
             t.sleep(0.5)
 
-    x, y = py.center(a)
     py.click(x, y)
-    py.scroll(-400)
-    findAndPrintAbsencesBox()
+    py.scroll(-2000)
+    extractAbsencesUsingCursor()
 
-def hasNext():
-    py.click()
-    py.click()
-    py.click()
-    py.hotkey('ctrl', 'shift', 'c')
+
+def getDisciplineName():
+    canSkip = False
+    while not canSkip:
+        try:
+            x, y = py.locateCenterOnScreen(resource_path('images/refdiscipline.png'), grayscale=True)
+            canSkip = True
+        except Exception as e:
+            time.sleep(0.5)
+
+    py.click(x, y+50)
+    py.click(x, y+50)
+    py.click(x, y+50)
+    py.hotkey('ctrl', 'c')
+    global currentDiscipline
+    currentDiscipline = cb.paste()
+
+
+def hasNext(x, y):
+    cb.copy('')
+    py.click(x,y)
+    py.click(x,y)
+    py.click(x,y)
+    py.hotkey('ctrl', 'c')
+    if 'turma' in cb.paste():
+        global ended
+        ended = True
+        return False
+    return True
 
 
 def goToFrequncy():
-    a = py.locateOnScreen('componente.png')
-    x, y = py.center(a)
-    py.click(x - 90, y + 25)
-
-    t.sleep(0.5)
-    canSkip = False;
+    canSkip = False
     while not canSkip:
         try:
-            a = py.locateOnScreen('frequencia.png')
+            x, y = py.locateCenterOnScreen(resource_path('images/componente.png'), grayscale=True)
+            canSkip = True
+        except Exception as e:
+            time.sleep(0.5)
+            print(e)
+
+    py.click(x, y)
+    py.scroll(-340)
+
+    canSkip = False
+    while not canSkip:
+        try:
+            x, y = py.locateCenterOnScreen(resource_path('images/componente.png'), grayscale=True)
+            canSkip = True
+        except Exception as e:
+            time.sleep(0.5)
+
+    if not hasNext(x - 200, y + 10 + ((i+1)*28)):
+        return
+
+    t.sleep(0.5)
+    getDisciplineName()
+
+    canSkip = False
+    while not canSkip:
+        try:
+            x, y = py.locateCenterOnScreen(resource_path('images/frequencia.png'), grayscale=True)
             canSkip = True
         except Exception as e:
             try:
-                a = py.locateOnScreen('alunos.png')
-                x, y = py.center(a)
+                x, y = py.locateCenterOnScreen(resource_path('images/alunos.png'), grayscale=True)
                 py.click(x, y)
             except Exception as b:
                 time.sleep(1)
 
-    x, y = py.center(a)
     py.click(x, y)
     t.sleep(0.5)
     rollDown()
 
-
-goToFrequncy()
+temp = cb.paste
+while not ended:
+    goToFrequncy()
+    i += 1
+cb.copy = cb.paste
 
 winsound.Beep(500, 1000)
+
+input("Press Enter to exit...")
